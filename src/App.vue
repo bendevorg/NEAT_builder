87f017d2b0987717596f63f1a7625485cb6bded3
leonardo a <leonardo.akato@gmail.com>
2018-05-20 11:51:48 -0300

    rename file fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Flappy msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Flappy from './components/Flappy.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Flappy
   }
 }
 </script>
