6c395c61d39dc8c80dde39f640be758a872d6dca
GuiMac <guibasconti@gmail.com>
2018-05-22 19:59:39 -0300

    Remove unused divs

--- src/components/Flappy.vue
+++ src/components/Flappy.vue
@@ -14,23 +14,6 @@
 
   <app-game-info/>
   <div class="row" id="gameSetup">
-    <div id="gameInfo" class="row" style="display:none;">
-      <div class="col-md-6">
-        <p>
-          speed:
-          <input id="speedSlider" type="range" min="1" max="10" value="1">
-          <span id="speed">1</span>
-          <br/> generation high score:
-          <span id="hs">0</span>
-          <br/> all time high score:
-          <span id="ahs">0</span>
-          <br/> steps:
-          <span id="stepsCount">0</span>
-          <br/> time spent:
-          <span id="timeSpent">0</span> seconds
-        </p>
-      </div>
-    </div>
     <app-instruction/>
     <app-input/>
   </div>
