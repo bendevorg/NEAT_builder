7e0d6da380f2f68d3970fe65e3d6421be9b50efb
GuiMac <guibasconti@gmail.com>
2018-05-22 14:38:51 -0300

    Remove unused components

--- src/App.vue
+++ src/App.vue
@@ -2,51 +2,27 @@
   <div id="app">
     <img src="./assets/logo.png">
     <Flappy msg="Welcome to Your Vue.js App"/>
-    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
-    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
   </div>
 </template>
 
 <script>
-  import Flappy from './components/Flappy.vue'
-  import QuoteGrid from './components/QuoteGrid.vue';
-  import NewQuote from './components/NewQuote.vue';
-  import Header from './components/Header.vue';
+import Flappy from "./components/Flappy.vue";
 
 export default {
-  name: 'app',
-        data: function () {
-            return {
-                quotes: [
-                    'Just a Quote to see something'
-                ],
-                maxQuotes: 10
-            }
-        },
-        methods: {
-            newQuote(quote) {
-                if (this.quotes.length >= this.maxQuotes) {
-                    return alert('Please delete Quotes first!');
-                }
-                this.quotes.push(quote);
-            },
-            deleteQuote(index) {
-                this.quotes.splice(index, 1);
-            }
-        },
-        components: {
-            Flappy,
-            appQuoteGrid: QuoteGrid,
-            appNewQuote: NewQuote,
-            appHeader: Header
-        }
-    }
+  name: "app",
+  data: function() {
+  },
+  methods: {
+  },
+  components: {
+    Flappy
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
