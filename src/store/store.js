639377b577c1f5dc9bb55af67d2eda9f7a69c826
leonardo a <leonardo.akato@gmail.com>
2018-05-22 17:32:44 -0300

    create gameVariable store obj

--- src/store/store.js
+++ src/store/store.js
@@ -5,40 +5,42 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    birdInput: [
-      { varname:'bird.x', description: 'Bird X position (Defined between 0 and the game.width)' },
-      { varname:'bird.y', description: 'Bird Y position (Defined between 0 and the game.height)' },
-      { varname:'bird.radius', description: 'Bird radius size' },
-      { varname:'bird.velocity', description: 'Bird velocity (velocity in the Y position is -5 and Max 5)' },
-      { varname:'bird.maxVelocity', description: 'Bird maximum velocity (5)' },
-      { varname:'bird.minVelocity', description: 'Bird minimum velocity (-5)' },
-      { varname:'bird.score', description: 'Actual bird score' },
-      { varname:'birds.scoreSum', description: 'All birds sums' },
-    ],
-    pipeInput:[
-      { varname: 'pipes.closest.x', description: 'Closest x pipe' },
-      { varname: 'pipes.closest.bottom', description: 'Closest pipe bottom entrance' },
-      { varname: 'pipes.closest.top', description: 'Closes pipe top entrance' },
-      { varname: 'pipes.closest.width', description: 'Closest pipe width' },
-      { varname: 'pipes.closest.velocity', description: 'Closest pipe velocity (fixed at 6)' },
-    ],
-    gameInput: [
-      { varname: 'game.height', description: 'Height size of game canvas' },
-      { varname: 'game.width', description: 'Width size of game canvas' }
-    ],
+    gameVariable:{
+      birdInput: [
+        { varname:'bird.x', description: 'Bird X position (Defined between 0 and the game.width)' },
+        { varname:'bird.y', description: 'Bird Y position (Defined between 0 and the game.height)' },
+        { varname:'bird.radius', description: 'Bird radius size' },
+        { varname:'bird.velocity', description: 'Bird velocity (velocity in the Y position is -5 and Max 5)' },
+        { varname:'bird.maxVelocity', description: 'Bird maximum velocity (5)' },
+        { varname:'bird.minVelocity', description: 'Bird minimum velocity (-5)' },
+        { varname:'bird.score', description: 'Actual bird score' },
+        { varname:'birds.scoreSum', description: 'All birds sums' },
+      ],
+      pipeInput: [
+        { varname: 'pipes.closest.x', description: 'Closest x pipe' },
+        { varname: 'pipes.closest.bottom', description: 'Closest pipe bottom entrance' },
+        { varname: 'pipes.closest.top', description: 'Closes pipe top entrance' },
+        { varname: 'pipes.closest.width', description: 'Closest pipe width' },
+        { varname: 'pipes.closest.velocity', description: 'Closest pipe velocity (fixed at 6)' },
+      ],
+      gameInput: [
+        { varname: 'game.height', description: 'Height size of game canvas' },
+        { varname: 'game.width', description: 'Width size of game canvas' }
+      ],
+    },
     neuralNetwork: {
       variableInputAmount: 1
     }
   },
   getters: {
     getBirdInput: state => {
-      return state.birdInput
+      return state.gameVariable.birdInput
     },
     getPipeInput: state => {
-      return state.pipeInput
+      return state.gameVariable.pipeInput
     },
     getGameInput: state => {
-      return state.gameInput
+      return state.gameVariable.gameInput
     },
     getVariableInputAmount: state => {
       return state.neuralNetwork.variableInputAmount
