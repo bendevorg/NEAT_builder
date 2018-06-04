78a03dbe7d06124e6510887d17e75a4c8c0cdc67
mmakarios <michel.makarios@gmail.com>
2018-06-03 21:48:06 -0300

    Group small inputs in same line

--- src/components/game/input/Input.vue
+++ src/components/game/input/Input.vue
@@ -23,23 +23,26 @@
             single-line
           />
         </div>
-        <v-text-field 
-          v-model="neuralNetwork.hiddenLayers" 
-          type="number" 
-          name="Hidden Layers" 
-          label="Hidden layers"
-          placeholder="5"
-        />
-        <v-text-field 
-          v-model="neuralNetwork.learningRate" 
-          type="number" 
-          name="Learning Rate" 
-          label="Learning rate"
-          min="0"
-          max="1"
-          step="0.1"
-          placeholder="0.2"
-        />
+        
+        <v-flex d-flex>
+          <v-text-field 
+            v-model="neuralNetwork.hiddenLayers" 
+            type="number" 
+            name="Hidden Layers" 
+            label="Hidden layers"
+            placeholder="5"
+          />
+          <v-text-field 
+            v-model="neuralNetwork.learningRate" 
+            type="number" 
+            name="Learning Rate" 
+            label="Learning rate"
+            min="0"
+            max="1"
+            step="0.1"
+            placeholder="0.2"
+          />
+        </v-flex>
         <!-- <v-text-field 
           label="Output Layers"
           value="2"
@@ -57,18 +60,20 @@
       class="row">
       <div class="col-md-6">
         <v-subheader>Genetic Algorithm</v-subheader>
-        <v-text-field 
-          v-model="genetic.population" 
-          type="number" 
-          label="Species per generation"
-          placeholder="500"
-        />
-        <v-text-field 
-          v-model="genetic.mutationRate" 
-          type="number" 
-          label="Mutation rate"
-          placeholder="0.01"
-        />
+        <v-flex d-flex>
+          <v-text-field 
+            v-model="genetic.population" 
+            type="number" 
+            label="Species per generation"
+            placeholder="500"
+          />
+          <v-text-field 
+            v-model="genetic.mutationRate" 
+            type="number" 
+            label="Mutation rate"
+            placeholder="0.01"
+          />
+        </v-flex>
       </div>
     </div>
 
