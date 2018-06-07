4048f5ffec250ae4c7060d745d2ba54cbeb54b54
mmakarios <michel.makarios@gmail.com>
2018-06-06 21:08:43 -0300

    Update instruction api

--- src/components/game/instruction/Instruction.vue
+++ src/components/game/instruction/Instruction.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="col-md-6" id="gameDescription">
-    <div v-for="instruction in getInstructions" :key="instruction.index">
-      <h3>{{instruction.name}}</h3>
+  <div id="gameDescription">
+    <div 
+      v-for="instruction in getInstructions" 
+      :key="instruction.index">
+      <h3>{{ instruction.name }}</h3>
       <ul>
-        <li v-for="item in instruction.items" :key="item.index">
+        <li 
+          v-for="item in instruction.items" 
+          :key="item.index">
           {{ item.name }} - {{ item.description }}
         </li>
       </ul>
@@ -12,36 +16,29 @@
 </template>
 
 <script>
-import API from '../../../utils/API.js';
+import API from '../../../utils/API';
 
 export default {
   name: 'Instructions',
-  props: {
-    msg: String
-  },
-  data: () => {
-    return {
-      instructions: []
-    }
-  },
+  data: () => ({
+    instructions: []
+  }),
   computed: {
-    getInstructions(){
+    getInstructions() {
       return this.instructions;
     }
   },
   mounted() {
-    API
-      .get(`/game/ ${this.$store.getters.gameId} /instructions`)
+    API.get(`/games/${this.$store.getters.gameId}/instructions`)
       .then(response => {
-        this.instructions = response.data.msg
+        this.instructions = response.data.msg;
       })
       .catch(err => {
         console.log(err);
       });
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="stylus" scoped>
 </style>
