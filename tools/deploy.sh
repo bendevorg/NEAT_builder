73d4f579d566830c2b2ee6a88b6d6a18971f9549
GuiMac <guibasconti@gmail.com>
2018-07-01 03:42:32 -0300

    Deploy comments

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,24 +1,29 @@
 create_folder() {
+  echo "Creating temp folder to hold our dist"
   ssh -i $3 $1@$2 "mkdir -p tmp"
 }
 
 clone_dist() {
+  echo "Cloning our dist to the remote server"
   scp -i $3 -r dist $1@$2:tmp
 }
 
 setup() {
+  echo "Setting up our server to new dist"
   forever stop $1
   cd github/$1
   git pull
 }
 
 replace_dist() {
+  echo "Updating dist"
   rm -rf dist
   mv ~/tmp/dist ./dist
   rm -rf ~/tmp/dist
 }
 
 update_server() {
+  echo "Restart server"
   forever --id $1 -a -l /dev/null start tools/startServer.js
 }
 
