2081ca6a8ddcf5a0e15e343ac8d8f40cedec4ea3
leonardo ak <leonardo.akato@gmail.com>
2018-07-02 01:31:20 -0300

    refactor: call nav item component

--- src/components/Shared/Navmenu.vue
+++ src/components/Shared/Navmenu.vue
@@ -9,19 +9,7 @@
         <v-subheader>
           Games
         </v-subheader>
-        <v-list-tile 
-          v-for="game in getGames" 
-          :key="game.index"
-          :to="{path: `/games/${game.name}`}">
-          <v-list-tile-action>
-            <v-icon left>label</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>
-              {{ game.name }}
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+        <app-navmenu-item />
       </v-list>
     </v-navigation-drawer>
     <v-toolbar
@@ -37,11 +25,13 @@
 
 <script>
 import Content from './Content';
+import NavmenuItem from './NavmenuItem';
 
 export default {
   name: 'Navmenu',
   components: {
-    appContent: Content
+    appContent: Content,
+    appNavmenuItem: NavmenuItem
   },
   data() {
     return {
