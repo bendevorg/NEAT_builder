e06c2d669ccc6386dba172c9683a1058a4d6c2ce
leonardo a <leonardo.akato@gmail.com>
2018-07-11 21:22:31 -0300

    fix fixed instruction size issue

--- src/components/game/instruction/Instruction.vue
+++ src/components/game/instruction/Instruction.vue
@@ -16,6 +16,7 @@
                   ripple
                   slot="activator"
                   @click="doCopy(item.name)"
+                  class="list-tile"
                 >
                   <v-list-tile-content>
                     <v-list-tile-title>{{ item.name }}</v-list-tile-title>
@@ -81,4 +82,10 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  .v-list__tile__sub-title {
+    white-space: normal;
+  }
+  .list-tile >>> .v-list__tile.v-list__tile--link{
+    height: 100%;
+  }
 </style>
